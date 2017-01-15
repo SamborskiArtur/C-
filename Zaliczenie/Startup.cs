4d396826d51ad6cff70f6acbc26283ed5f75b7e3
Artur <Artur@ARTUR-KOMPUTER>
2017-01-15 23:18:58 +0100

    Add project files.

---
+++ Zaliczenie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Zaliczenie.Startup))]
+namespace Zaliczenie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
